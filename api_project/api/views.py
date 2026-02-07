ca169f95a727d721d139869fbf8a9737e2c2505b
Zephan3s1116 <Zephan3s1116@gmail.com>
2026-02-07 20:39:37 +0300

    Implement CRUD operations with BookViewSet and DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -6,6 +7,22 @@ class BookList(generics.ListAPIView):
     """
     API view to retrieve list of books.
     GET /api/books/ - Returns a list of all books
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A ViewSet for performing CRUD operations on Book model.
+    
+    Provides the following actions:
+    - list: GET /api/books_all/ - List all books
+    - create: POST /api/books_all/ - Create a new book
+    - retrieve: GET /api/books_all/<id>/ - Retrieve a specific book
+    - update: PUT /api/books_all/<id>/ - Update a book
+    - partial_update: PATCH /api/books_all/<id>/ - Partially update a book
+    - destroy: DELETE /api/books_all/<id>/ - Delete a book
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
